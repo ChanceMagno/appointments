8f3dbd7b4e4f5ec073e693f63beb9bf21c6a8676
Chance Magno and Nathaniel Meyer <nathaniel@>
2017-02-15 13:32:40 -0800

    Delete commented code

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,14 @@
 $(document).ready(function() {
   $("form#appointmentBooking").submit(function(event) {
-
     var customerName =$("input#name").val();
-    alert(customerName);
     var date = $("input#date").val();
     var startTime = $("input#startTime").val();
     var endTime = $("input#endTime").val();
-
-      $(".customerName").text(customerName);
-      $(".customerDate").text(date);
-      $(".setTime").text(startTime);
-      $(".setEndTime").text(endTime);
-      $("#appointmentConfirmation").show();
-
-      event.preventDefault();
-
-      alert("end");
+    $(".customerName").text(customerName);
+    $(".customerDate").text(date);
+    $(".setTime").text(startTime);
+    $(".setEndTime").text(endTime);
+    $("#appointmentConfirmation").show();
+    event.preventDefault();
   });
-
 });
-
-/*
-var weight = parseInt(prompt("What is your weight in kilograms?"));
-var height = parseInt(prompt("What is your height in meters?"));
-var calculateBMI = function(weight,height){
-	return(weight / height / height);
-};
-alert("Your BMI is " + calculateBMI(weight,height) + ". Go to the hospital."); */
-
-
-/*
-var fahrenheit = parseInt(prompt("Enter fahrenheit: "));
-var celsius = parseInt(prompt("Enter celcius: "));
-
-var celsiusToFahrenheit = function(cel){
-	return (cel*(9/5)) +32;
-}
-
-var fahrenheitToCelsius = function(fah){
-	return (fah-32) * (5/9);
-};
-
-alert("Celsius to fahrenheit is: " + celsiusToFahrenheit(celsius));
-alert("Fahrenheit to celsius is: " + fahrenheitToCelsius(fahrenheit)); */
